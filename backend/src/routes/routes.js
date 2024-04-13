279b0e67416d1825eaec8d0db3499d0a6334ba3d
nandhu-44 <nandhu90040@gmail.com>
2024-04-13 12:58:40 +0530

    Removed old code structure

--- backend/src/routes/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const users = require('./users/index');
-
-router.use('/users', users);
-
-module.exports = router;    
--- frontend/src/database/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const { REACT_APP_SUPABASE_URL: supabaseUrl, REACT_APP_SUPABASE_API_KEY: supabaseApiKey } = process.env;
-const supabaseClient = createClient(supabaseUrl, supabaseApiKey);
-
-export default supabaseClient;
